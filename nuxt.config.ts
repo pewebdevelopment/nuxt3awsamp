9bbf0839a575521eee5d39ce823d9b62c0a491f8
pewebdevelopment <76696556+pewebdevelopment@users.noreply.github.com>
2022-10-08 10:45:54 +0530

    edited the nuxtconfig file for settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,6 +12,10 @@ export default defineNuxtConfig({
     
   ],
 
+
+  //Sometimes polyfill.js may throw an error at buildtime. That error reference is mentioned below:
+  // https://github.com/nuxt/framework/discussions/2308#discussioncomment-1822271
+
   // meta: {
   //   script: [
   //     { src: './js/polyfills.js' },
@@ -29,19 +33,22 @@ export default defineNuxtConfig({
     },
   },
 
+
+  // Nuxt 3 uses Vite under the hood, hence we have to use the following to configure vite settings
   vite: {
     resolve: {
         alias: {
             './runtimeConfig': './runtimeConfig.browser'
         }
     },
+
     // temp-fix for dev, it breaks build for now (see: https://github.com/nuxt/framework/issues/4916)
     // define: {
     //     global: {}
     // }
 
     define: {
-      'window.global': {}
+      'window.global': {} // using window.global is much better as stated here: https://github.com/nuxt/framework/discussions/2308#discussioncomment-2766426
     }
 }
 });
